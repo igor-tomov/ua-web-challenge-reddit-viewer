f007af1eec8ced19930c526fad5bccec8baac46f
stepennwolf <stepennwolf@gmail.com>
2015-04-14 22:37:49 +0300

    Added base structure of project

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        less: {
+            options:{
+                paths: ["app/styles"]
+            },
+
+            dist: {
+                files: {
+                    "app/styles/build/bundle.css": "app/styles/less/index.less"
+                }
+            }
+        },
+
+        browserify: {
+            dist: {
+                files: {
+                    'app/js/build/bundle.js': 'app/js/app.js'
+                }
+            }
+        },
+
+        react: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "app/js",
+                        src: [ "components/*.react.jsx" ],
+                        dest: "app/js",
+                        ext: ".react.js"
+                    }
+                ]
+            }
+        },
+
+        watch: {
+            js: {
+                files: [
+                    'app/js/**/*.js'
+                ],
+                tasks: ['react:dist', 'browserify:dist']
+            },
+            styles: {
+                files: [
+                    'app/styles/less/**/*.less'
+                ],
+                tasks: ['less:dist']
+            }
+        }
+    });
+};
---
+++ app/js/app.js
@@ -0,0 +1,1 @@
+console.log( "App has been started..." );
