c9712cc848cb86e5e32e23525e373a650d0d8008
Igor Tomov <igor.tomov@globallogic.com>
2015-04-16 18:26:36 +0300

    Intermediate commit

--- app/js/app.js
+++ app/js/app.js
@@ -10,19 +10,40 @@ if ( ! ServiceWorkerAdapter.isSupported() ){
 var serviceWorker = new ServiceWorkerAdapter( 'service-worker.js' );
 
 // init reddit
+reddit.about('javascript').fetch(function(response){
+  console.log( "about:", response );
+});
+
 reddit.hot('javascript').limit(25).fetch(function( response ){
   var articles = response.data.children.slice(1);
 
-  articles = articles.map(function( item ){
-    var data = item.data;
+  // fetch comments for each article in target subreddit
+  var comments = [];
 
-    return {
-      title: data.title,
-      author: data.author,
-      url: data.url,
-      created: data.created
-    }
+  articles.forEach(function( item ){
+    var promise = new Promise(function( resolve ){
+      reddit.comments( item.data.id, 'javascript' ).sort( "hot" ).fetch(function( response ) {
+        resolve( response );
+      });
+    });
+
+    comments.push( promise );
   });
 
-  console.log( articles );
+  // Reduce articles data
+  Promise.all( comments ).then(function( comments ){
+    articles = articles.map(function( item, i ){
+      var data = item.data;
+
+      return {
+        title: data.title,
+        author: data.author,
+        url: data.url,
+        created: data.created,
+        comments: comments[i]
+      }
+    });
+
+    console.log( articles );
+  });
 });
